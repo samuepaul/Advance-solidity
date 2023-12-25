cd3158fbc24e9f57d94abb9719d401ea5c1bbc0d
Samuel Paul <dogarapaul91@gmail.com>
2023-12-25 15:10:30 +0100

    add all

--- SecurityAuditsProject/hardhat.config.js
+++ SecurityAuditsProject/hardhat.config.js
@@ -11,7 +11,7 @@ module.exports = {
     // set mumbai testnet for deploying smart contract
     mumbai: {
       url: "https://holy-omniscient-meme.matic-testnet.discover.quiknode.pro/844a88e15be1e30d1f855bc750b2401cc57b3089/",
-      accounts: ["9673488150c05380c2d245a4b7926252132489ecc9f19fd3513e926993cce2d1"],
+      accounts: ["Private Key here"],
     }
   },
   solidity: {
